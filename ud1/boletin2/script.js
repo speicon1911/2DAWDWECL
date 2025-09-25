4f78c5be08b73136bda768a9afdc16ba513d3aa7
Salvador <salvadorpeinado111906@gmail.com>
2025-09-25 12:41:10 +0200

    Refactor HTML structure and fix JavaScript function errors for Fibonacci and factorial calculations

--- ud1/boletin2/script.js
+++ ud1/boletin2/script.js
@@ -3,9 +3,42 @@ let colores = ["#FFD1DC", "#AEC6CF", "#77DD77", "#F49AC2", "#FFB347", "#CBAACB",
 
 let ejercicios = document.getElementsByClassName("ejercicio");
 
-window.onload = function() {
-    for(let i = 0; i = ejercicios.length; i++){
+window.onload = function () {
+    for (let i = 0; i < ejercicios.length; i++) {
         let colorAleatorio = Math.random() * colores.length;
-        ejercicios[i].style.backgroundColor = colores[Math.floor.colorAleatorio];
+        ejercicios[i].style.backgroundColor = colores[Math.floor(colorAleatorio)];
     }
+}
+
+function calcular_fibonacci() {
+    let num = Number(prompt("Introduce un numero"));
+    console.log("El valor fib(" + num + ") es: " + fibonacci(num));
+}
+function fibonacci(num) {
+    let resultado;
+    // Comprobamos los casos base
+    if (num === 0) {
+        resultado = 0;
+    } else if (num === 1) {
+        resultado = 1;
+    } else {
+        // Aqui esta la llamda recursiva
+        resultado = fibonacci(num - 1) + fibonacci(num - 2);
+    }
+    return resultado;
+}
+
+function calcular_factorial() {
+    let num = Number(prompt("Introduce un numero"));
+    console.log("El valor fact(" + num + ") es: " + factorial(num));
+}
+
+function factorial(num) {
+    let resultado;
+    if (num === 1) {
+        resultado = 1;
+    } else {
+        resultado = num * factorial(num - 1);
+    }
+    return resultado;
 }
