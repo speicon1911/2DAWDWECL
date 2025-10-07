3f441735227d2d007873b7af7f7905b9c2bfbf99
Salvador <salvadorpeinado111906@gmail.com>
2025-10-07 13:20:52 +0200

    actualizar
    Falta corregir lo que ha realizado octavio en clase

--- ud1/PruebaPracticaDados/script.js
+++ ud1/PruebaPracticaDados/script.js
@@ -1,11 +1,24 @@
-let nombre;
-var edad;
-var activaJuego = false;
+// Constantes
+const CARAS = 6;
+const LIMIT = 3;
 
+// Variables globales
+let nombre;
+let edad;
+let activaJuego = false;
+let sumaTotal = 0;
+let cuentaTiradas = 0;
+let max = 0;
+let min = Infinity;
+let f_inicio = null;
+let f_fin = null;
+
+// Funciones
 function iniciar_juego() {
     console.log("Se ha iniciado el juego");
     nombre = prompt("Introduce tu nombre:");
     edad = Number(prompt("Introduce tu edad:"));
+    f_inicio = new Date();
     if (nombre && edad.valueOf(Number)) {
         activaJuego = true;
     } else {
@@ -14,17 +27,15 @@ function iniciar_juego() {
 }
 
 function lanzar_dados() {
-    var cuentaTiradas = 0;
     var sumaPuntos = 0;
-    const maxTiradas = 3;
 
     if (activaJuego === true) {
         console.log("Lanzando dados...");
         do {
-            var dado1 = Math.floor(Math.random() * 6) + 1;
+            var dado1 = Math.floor(Math.random() * CARAS) + 1;
             // var dado2 = dado1;
-            var dado2 = Math.floor(Math.random() * 6) + 1;
-            var sumaTotal = dado1 + dado2;
+            var dado2 = Math.floor(Math.random() * CARAS) + 1;
+            sumaTotal = dado1 + dado2;
             sumaPuntos += sumaTotal;
             cuentaTiradas++;
 
@@ -33,8 +44,8 @@ function lanzar_dados() {
             console.log("El jugador " + nombre + " ha obtenido " + sumaPuntos + " en esta tirada.");
             console.log("Llevas: " + cuentaTiradas + " tiradas");
 
-        } while (dado1 === dado2 && cuentaTiradas <= maxTiradas);
-        if (cuentaTiradas > maxTiradas) {
+        } while (dado1 === dado2 && cuentaTiradas <= LIMIT);
+        if (cuentaTiradas > LIMIT) {
             sumaTotal = 0;
             console.log("Has llegado a mas de 3 tiradas por lo que se reinicia tu progreso")
             sumaPuntos = 0;
@@ -44,8 +55,27 @@ function lanzar_dados() {
     } else {
         console.error("Debes iniciar el juego primero");
     }
+
+    // Actualizamos estadistica
+    if (sumaPuntos > max) {
+        max = sumaPuntos;
+    }
+    if (sumaPuntos < min) {
+        min = sumaPuntos;
+    }
+
 }
 
-function mostrar_estadistica(){
+function mostrar_estadistica() {
     console.log("Se ha tirado " + cuentaTiradas + " veces");
+    console.log("El valor minimo obtenido es " + min);
+    console.log("El valor maximo obtenido es " + max);
+    console.log("La puntuacion media por tirada es " + (sumaTotal / cuentaTiradas).toFixed(2));
+
+}
+
+function salir() {
+    activaJuego = false;
+    f_fin = new Date();
+    console.log("Fin del juego. Has estado jugando " + ((f_fin - f_inicio)/1000) + " segundos");
 }
